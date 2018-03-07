31d14062f30a60888a7ab53a0e9fa4ddaa1e9da5
Greg Spitz <gregspitz6@gmail.com>
2018-03-07 12:56:17 -0500

    Add tests and implementation for FlashcardRepository

--- src/main/java/com/gregspitz/springflashcardserver/data/FlashcardRepository.java
+++ src/main/java/com/gregspitz/springflashcardserver/data/FlashcardRepository.java
@@ -2,40 +2,51 @@ package com.gregspitz.springflashcardserver.data;
 
 import com.gregspitz.springflashcardserver.model.Flashcard;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FlashcardRepository implements FlashcardDataSource {
 
-    // TODO: fill this in and test
+    private Map<String, Flashcard> flashcards;
+
+    public FlashcardRepository() {
+        flashcards = new HashMap<>();
+    }
 
     @Override
     public List<Flashcard> getFlashcards() {
-        return Arrays.asList(new Flashcard("Front", "Back"));
+        return new ArrayList<>(flashcards.values());
     }
 
     @Override
     public Flashcard getFlashcardById(String id) {
-        return null;
+        return flashcards.get(id);
     }
 
     @Override
     public void deleteFlashcard(String id) {
-
+        flashcards.remove(id);
     }
 
     @Override
     public void deleteFlashcard(Flashcard flashcard) {
-
+        if (flashcard != null) {
+            deleteFlashcard(flashcard.getId());
+        }
     }
 
     @Override
     public void addFlashcard(Flashcard flashcard) {
-
+        if (flashcard != null) {
+            flashcards.put(flashcard.getId(), flashcard);
+        }
     }
 
     @Override
     public void addFlashcards(List<Flashcard> flashcards) {
-
+        for (Flashcard flashcard : flashcards) {
+            if (flashcard != null) {
+                this.flashcards.put(flashcard.getId(), flashcard);
+            }
+        }
     }
 }
--- src/test/java/com/gregspitz/springflashcardserver/data/FlashcardRepositoryTest.java
+++ src/test/java/com/gregspitz/springflashcardserver/data/FlashcardRepositoryTest.java
@@ -1,7 +1,16 @@
 package com.gregspitz.springflashcardserver.data;
 
+import com.gregspitz.springflashcardserver.model.Flashcard;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.gregspitz.springflashcardserver.TestData.EXPECTED_FLASHCARDS;
+import static com.gregspitz.springflashcardserver.TestData.FLASHCARD_1;
+import static com.gregspitz.springflashcardserver.TestData.FLASHCARD_2;
+import static org.junit.Assert.assertEquals;
 
 public class FlashcardRepositoryTest {
 
@@ -13,7 +22,76 @@ public class FlashcardRepositoryTest {
     }
 
     @Test
-    public void canAddAndRetrieveFlashcards() {
+    public void canAddFlashcardsIndividuallyAndRetrieveFlashcards() {
+        repository.addFlashcard(FLASHCARD_1);
+        repository.addFlashcard(FLASHCARD_2);
+        List<Flashcard> flashcards = repository.getFlashcards();
+        assertEquals(EXPECTED_FLASHCARDS, flashcards);
+    }
 
+    @Test
+    public void canAddListOfFlashcardsAndRetrieveFlashcards() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        List<Flashcard> flashcards = repository.getFlashcards();
+        assertEquals(EXPECTED_FLASHCARDS, flashcards);
+    }
+
+    @Test
+    public void wontAddNullFlashcardIndividually() {
+        repository.addFlashcard(null);
+        List<Flashcard> flashcards = repository.getFlashcards();
+        assertEquals(0, flashcards.size());
+    }
+
+    @Test
+    public void wontAddNullFlashcardByGroup() {
+        List<Flashcard> nullList = Arrays.asList(FLASHCARD_1, null);
+        repository.addFlashcards(nullList);
+        List<Flashcard> flashcards = repository.getFlashcards();
+        assertEquals(1, flashcards.size());
+    }
+
+    @Test
+    public void canRetrieveFlashcardById() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        Flashcard retrievedFlashcard = repository.getFlashcardById(FLASHCARD_1.getId());
+        assertEquals(FLASHCARD_1, retrievedFlashcard);
+    }
+
+    @Test
+    public void canDeleteFlashcard() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        repository.deleteFlashcard(FLASHCARD_1);
+        List<Flashcard> flashcards = repository.getFlashcards();
+        assertEquals(1, flashcards.size());
+    }
+
+    @Test
+    public void canDeleteFlashcardById() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        repository.deleteFlashcard(FLASHCARD_1.getId());
+        List<Flashcard> flashcards = repository.getFlashcards();
+        assertEquals(1, flashcards.size());
+    }
+
+    @Test
+    public void wontThrowExceptionOnNullFlashcardDeletion() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        Flashcard nullFlashcard = null;
+        repository.deleteFlashcard(nullFlashcard);
+    }
+
+    @Test
+    public void wontThrowExceptionOnNullIdDeletion() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        String nullId = null;
+        repository.deleteFlashcard(nullId);
+    }
+
+    @Test
+    public void getReturnsNullForUnusedId() {
+        repository.addFlashcards(EXPECTED_FLASHCARDS);
+        Flashcard flashcard = repository.getFlashcardById(null);
+        assertEquals(null, flashcard);
     }
 }
